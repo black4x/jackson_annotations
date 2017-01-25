041e80bcb37c2b5d5f57da68917be4b6068ce462
Andrius Juodelis <juodelis@puzzle.ch>
2017-01-25 13:51:07 +0100

    add @JsonFormat

--- src/main/java/dto/jsonformat/Event.java
+++ src/main/java/dto/jsonformat/Event.java
@@ -1,4 +1,14 @@
 package dto.jsonformat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
 public class Event {
+    public String name;
+
+    @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "dd-MM-yyyy hh:mm:ss")
+    public Date eventDate;
 }
--- src/test/java/dto/jsonformat/EventTest.java
+++ src/test/java/dto/jsonformat/EventTest.java
@@ -1,5 +1,29 @@
-import static org.junit.Assert.*;
+package dto.jsonformat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
 
 public class EventTest {
+
+    @Test
+    public void testDateFormat() throws JsonProcessingException, ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        String toParse = "25-12-2017 12:00:00";
+
+        Event event = new Event();
+        event.eventDate = simpleDateFormat.parse(toParse);
+        String json = new ObjectMapper().writeValueAsString(event);
+        assertThat(json, containsString(toParse));
+    }
 
 }
